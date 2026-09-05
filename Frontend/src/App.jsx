3edf86e15bacf18e11d27451a499a83d7dfbb1d6
Hasan Raza <hasan.raza2193@gmail.com>
2026-09-05 09:22:18 +0530

    add role update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
+import RoleSelect from "./pages/RoleSelect";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/roles" element={<RoleSelect />} />
 
-        <Route path="/roles" element={<h1>Choose Your Role</h1>} />
         <Route path="/admin" element={<h1>Admin Dashboard</h1>} />
         <Route path="/creator" element={<h1>Creator Dashboard</h1>} />
         <Route path="/student" element={<h1>Student Dashboard</h1>} />
